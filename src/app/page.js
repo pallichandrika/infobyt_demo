832eb28b64c0304180d5b31975d4c28572ccc4e9
Chandrika <palli.chandrika2003@gmail.com>
2024-07-24 09:50:28 +0530

    feat: First commit

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Login from "./signin/page";
+ 
+
+export default function Home() {
+  return (
+    <main className="">
+      
+     <Login/>
+ 
+   
+ 
+    </main>
+  );
+}
